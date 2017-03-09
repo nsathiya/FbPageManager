eb4fd484a558a0ddd78fa7646645bd83f18315a9
Naren Sathiya <Naren Sathiya nsathiya@spawnd.com>
2017-03-08 20:19:12 -0800

    first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,12 +23,8 @@ class Header extends React.Component {
           <Navigation />
           <Link className={s.brand} to="/">
             <img src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>Tax Expert Advice</span>
           </Link>
-          <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
-          </div>
         </div>
       </div>
     );
